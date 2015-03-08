d7509ce092a67fa3e2e7f17a23c9e90458498066
stansn96 <smwsnijders96@gmail.com>
2015-03-08 16:00:16 +0100

    nieuwste werkende versie van de spontal opdracht

--- spontal.py
+++ spontal.py
@@ -1,23 +1,22 @@
 #!/usr/bin/env python
 
 import xml.etree.ElementTree as ET
-tree = ET.parse('spontal.xml')
-root = tree.getroot()
 import sys
 
 def main():
 	
+	tree = ET.parse('spontal.xml')
+	root = tree.getroot()
+	
 	for point in root.findall('POINT'):
-		end = (point.find('F0_END').text)
-		start = (point.find('F0_START').text)
-		bottom = (point.find('BOTTOM_HZ').text)
-		top = (point.find('TOP_HZ').text)
-		if end < bottom or end > top:
-			root.remove(point[12])
-		if start < bottom or start > top:
-			root.remove(point[13])
-
-	tree.write('output.xml')
+		end = float((point.find('F0_END').text))
+		start = float((point.find('F0_START').text))
+		bottom = int((point.find('BOTTOM_HZ').text))
+		top = int((point.find('TOP_HZ').text))
+		if (not bottom <= start <= top) or (not bottom <= end <= top):
+			root.remove(point)
+	
+	tree.write('output1.xml')
 
 if __name__ == '__main__':
 	main()
